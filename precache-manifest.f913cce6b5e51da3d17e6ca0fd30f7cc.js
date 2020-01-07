a2e061a4b66eae7a9f41427991edc0447caa7269
revortni <rojit.blaze13@gmail.com>
2020-01-07 13:17:18 +0545

    Updates

---
+++ precache-manifest.f913cce6b5e51da3d17e6ca0fd30f7cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af10b23ee3931784ead52e4e51a9ac4f",
+    "url": "/react_note/index.html"
+  },
+  {
+    "revision": "de82244c1cb016bf4b59",
+    "url": "/react_note/static/css/main.09495593.chunk.css"
+  },
+  {
+    "revision": "49a17db8d94233239ef6",
+    "url": "/react_note/static/js/2.7a27f260.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_note/static/js/2.7a27f260.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de82244c1cb016bf4b59",
+    "url": "/react_note/static/js/main.f58eaeb4.chunk.js"
+  },
+  {
+    "revision": "2fe6fd2ca6e40051a57a",
+    "url": "/react_note/static/js/runtime-main.43ef50e0.js"
+  }
+]);
